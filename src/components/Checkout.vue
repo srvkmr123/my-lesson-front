9ea22dd94d10c04e44d998bd7dd993c9f0a97ea9
sourav kumar <srvdhiman1998@gmail.com>
2023-06-10 14:51:39 +0530

    first commit

---
+++ src/components/Checkout.vue
@@ -0,0 +1,29 @@
+<template>
+     <h2 style="text-align: center;">Welcome to checkout page!! </h2>
+    <div v-if="this.lessons.length!==0" class="lesson-container">
+        <div  v-for="item in this.lessons" :key="item._id" class="lessons">
+           <LessonComp :data="item" @removeId="removeId($event)" :pageNo="this.pageNo" :cspaces="this.cspaces"/>
+        </div>
+    </div>
+    <h1 v-else style="text-align: center;">Cart is Empty!! Add Lessons to cart</h1>
+</template>
+
+<script>
+import LessonComp from './LessonComp.vue'
+    export default {
+        name:"Checkout",
+        props:['msg','lessons','pageNo','cspaces'],
+        components:{LessonComp},
+        emits:['rId'],
+        methods:{
+            removeId(id){
+                this.$emit('rId',id)
+            }
+        },
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/LessonComp.vue
@@ -0,0 +1,50 @@
+<template>
+    <!-- <p>{{ this.lspaces }}</p> -->
+    <div class="lesson-comp">
+       <p >Name: {{ this.data.name }}</p>
+       <p>Price: {{ this.data.price }}</p> 
+       <div v-if="this.pageNo===1" v-for="items in this.lspaces" key="items._id">
+          <p v-if="items._id===this.data._id">Spaces: {{ items.spaces}}</p>
+       </div>
+       <div v-if="this.pageNo===2" v-for="items in this.cspaces" key="items._id">
+          <p v-if="items._id===this.data._id">spaces: {{ items.spaces}}</p>
+       </div>
+      
+       <button @click="handleAdd" v-if="this.pageNo===1"  id="add-btn" >Add to cart</button>
+       <button @click="handleRemove" v-if="this.pageNo===2" id="remove-btn">remove</button>
+    </div>
+</template>
+
+<script>
+import { isProxy, toRaw } from 'vue';
+    export default {
+        name:'LessonComp',
+        props:['data','addId','removeId','pageNo','lspaces','cspaces'],
+        methods:{
+            handleAdd:function(){
+                
+                this.$emit('addId',this.data._id)
+                this.$forceUpdate()
+            },
+            handleRemove(){
+                this.$emit('removeId',this.data._id)
+                this.$forceUpdate()
+            }
+        },
+        data(){
+            return{
+                lchanges:false
+            }
+        },
+        watch:{
+            lspaces:function(){
+                console.log('lspaces changed')
+            }
+        }
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+ 
+</style>
